38bef082f1f472ca6844214f0d53ab9354911980
Masumraihan <mdmasumraihan1@gmail.com>
2024-06-29 18:21:36 +0600

    update SEO

--- src/Providers/Providers.tsx
+++ src/Providers/Providers.tsx
@@ -1,35 +1,13 @@
-import ogImage from "../assets/og-image.png";
-import { Provider } from "react-redux";
-import store from "../redux/store";
 import { ConfigProvider } from "antd";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
       <HelmetProvider>
         <Helmet>
           <title>Product Management</title>
-          <meta
-            name='description'
-            content='Browse and manage our extensive collection of products. Find the latest and best products in our inventory.'
-          />
-          <meta name='keywords' content='products, product management, inventory, e-commerce' />
-          <link rel='canonical' href={import.meta.env.BASE_URL} />
-          <meta property='og:title' content='Product Management' />
-          <meta
-            property='og:description'
-            content='Browse and manage our extensive collection of products. Find the latest and best products in our inventory.'
-          />
-          <meta property='og:url' content={import.meta.env.BASE_URL} />
-          <meta property='og:type' content='website' />
-          <meta property='og:image' content={ogImage} />
-          <meta name='twitter:card' content='summary_large_image' />
-          <meta name='twitter:title' content='Product Management' />
-          <meta
-            name='twitter:description'
-            content='Browse and manage our extensive collection of products. Find the latest and best products in our inventory.'
-          />
-          <meta name='twitter:image' content={ogImage} />
         </Helmet>
         <ConfigProvider
           button={{ className: "my-button" }}
